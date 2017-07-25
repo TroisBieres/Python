26f01f8fa41f637b7b04f50628085245d0cc8e59
antony <antony.turpin@gmail.com>
2017-07-25 07:19:38 +0200

    Exercice 1 et 2

---
+++ 2_PythonEnModeProgrammation/06_LesInstructionsConditionnelles/Exercice1.py
@@ -0,0 +1,6 @@
+x = eval(input("Entrez une valeur pour x: "))
+if x < 2:
+    y=4-2*x
+else:
+    y=2*x-4
+print("y =",y)
---
+++ 2_PythonEnModeProgrammation/06_LesInstructionsConditionnelles/Exercice2.py
@@ -0,0 +1,5 @@
+x=eval(input('Entrez une valeur x: '))
+if x == int(x):
+    print(x, "est un entier")
+else:
+    print(x, "n'est pas un entier")
